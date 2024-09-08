e1526b75bef765ad773d992108a16d928d326eb1
Andre Torres <atcorrea17@gmail.com>
2024-09-08 18:21:28 -0300

    remove test boilerplate

--- test/DotlancheAuthentication.Tests/FunctionsTest.cs
+++ test/DotlancheAuthentication.Tests/FunctionsTest.cs
@@ -7,21 +7,4 @@ namespace DotlancheAuthentication.Tests;
 
 public class FunctionsTest
 {
-
-    public FunctionsTest()
-    {
-        // var mock = new Mock<ICalculatorService>();
-        // mock.Setup(m => m.Add(It.IsAny<int>(), It.IsAny<int>())).Returns(12);
-
-        // _mockCalculatorService = mock.Object;
-    }
-
-    [Fact]
-    public void TestAdd()
-    {
-        // TestLambdaContext context = new TestLambdaContext();
-        
-        // var functions = new Functions(_mockCalculatorService);
-        // Assert.Equal(12, functions.Add(3, 9, context));
-    }
 }
