d8484c5a4f78e1fd0c16513ca6a2e8f65b16bdff
Andre Torres <atcorrea17@gmail.com>
2024-09-22 11:10:46 -0300

    registering routes for dotlanche api (wip)

--- IaC/apigateway.tf
+++ IaC/apigateway.tf
@@ -1,4 +1,3 @@
-
 resource "aws_apigatewayv2_api" "apigateway" {
   name          = "dotlanches-api-gateway"
   protocol_type = "HTTP"
@@ -38,84 +37,4 @@ output "api_gateway_url" {
   description = "Base URL for API Gateway"
 
   value = aws_apigatewayv2_stage.apigateway-stage.invoke_url
-}
-
-################## GET USER ##############################
-
-resource "aws_apigatewayv2_integration" "getuser" {
-  api_id = aws_apigatewayv2_api.apigateway.id
-
-  integration_uri    = aws_lambda_function.getuser.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "getuser" {
-  api_id = aws_apigatewayv2_api.apigateway.id
-
-  route_key = "GET /get-user/{cpf}"
-  authorization_type = "JWT"
-  authorizer_id = aws_apigatewayv2_authorizer.management-authorizer.id
-  target    = "integrations/${aws_apigatewayv2_integration.getuser.id}"
-}
-
-resource "aws_lambda_permission" "getuser" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.getuser.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.apigateway.execution_arn}/*/*"
-}
-
-################## SIGN UP ##############################
-
-resource "aws_apigatewayv2_integration" "signup" {
-  api_id = aws_apigatewayv2_api.apigateway.id
-
-  integration_uri    = aws_lambda_function.signup.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "signup" {
-  api_id = aws_apigatewayv2_api.apigateway.id
-
-  route_key = "POST /sign-up"
-  target    = "integrations/${aws_apigatewayv2_integration.signup.id}"
-}
-
-resource "aws_lambda_permission" "signup" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.signup.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.apigateway.execution_arn}/*/*"
-}
-
-################## SIGN IN ##############################
-
-resource "aws_apigatewayv2_integration" "signin" {
-  api_id = aws_apigatewayv2_api.apigateway.id
-
-  integration_uri    = aws_lambda_function.signin.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "signin" {
-  api_id = aws_apigatewayv2_api.apigateway.id
-
-  route_key = "POST /sign-in"
-  target    = "integrations/${aws_apigatewayv2_integration.signin.id}"
-}
-
-resource "aws_lambda_permission" "signin" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.signin.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.apigateway.execution_arn}/*/*"
 }
---
+++ IaC/apigateway_integrations.tf
@@ -0,0 +1,66 @@
+################### GET USER LAMBDA
+
+resource "aws_apigatewayv2_integration" "getuser" {
+  api_id = aws_apigatewayv2_api.apigateway.id
+
+  integration_uri    = aws_lambda_function.getuser.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_lambda_permission" "getuser" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.getuser.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.apigateway.execution_arn}/*/*"
+}
+
+################### SIGN UP LAMBDA
+
+resource "aws_apigatewayv2_integration" "signup" {
+  api_id = aws_apigatewayv2_api.apigateway.id
+
+  integration_uri    = aws_lambda_function.signup.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_lambda_permission" "signup" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.signup.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.apigateway.execution_arn}/*/*"
+}
+
+################### SIGN IN LAMBDA
+
+resource "aws_apigatewayv2_integration" "signin" {
+  api_id = aws_apigatewayv2_api.apigateway.id
+
+  integration_uri    = aws_lambda_function.signin.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_lambda_permission" "signin" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.signin.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.apigateway.execution_arn}/*/*"
+}
+
+################### DOTLANCHES API
+
+resource "aws_apigatewayv2_integration" "dotlanches_api" {
+  api_id = aws_apigatewayv2_api.apigateway.id
+
+  integration_uri    = "TO DO"
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
